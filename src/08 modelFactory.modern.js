99911ce56dd4caec0582623a803092aac9287825
RubyLouvre <cheng19840218@gmail.com>
2015-06-01 18:53:49 +0800

    add avalon2

---
+++ src/08 modelFactory.modern.js
@@ -0,0 +1,293 @@
+/*********************************************************************
+ *                           modelFactory                             *
+ **********************************************************************/
+//avalon最核心的方法的两个方法之一（另一个是avalon.scan），返回一个ViewModel(VM)
+var VMODELS = avalon.vmodels = {} //所有vmodel都储存在这里
+avalon.define = function (id, factory) {
+    var $id = id.$id || id
+    if (!$id) {
+        log("warning: vm必须指定$id")
+    }
+    if (VMODELS[$id]) {
+        log("warning: " + $id + " 已经存在于avalon.vmodels中")
+    }
+    if (typeof id === "object") {
+        var model = modelFactory(id)
+    } else {
+        var scope = {
+            $watch: noop
+        }
+        factory(scope) //得到所有定义
+        model = modelFactory(scope) //偷天换日，将scope换为model
+        stopRepeatAssign = true
+        factory(model)
+        stopRepeatAssign = false
+    }
+    model.$id = $id
+    return VMODELS[$id] = model
+}
+
+//一些不需要被监听的属性
+var $$skipArray = String("$id,$watch,$unwatch,$fire,$events,$model,$skipArray,$proxy").match(rword)
+
+function modelFactory(source, $special, $model) {
+    if (Array.isArray(source)) {
+        var arr = source.concat()
+        source.length = 0
+        var collection = arrayFactory(source)// jshint ignore:line
+        collection.pushArray(arr)
+        return collection
+    }
+    //0 null undefined || Node || VModel(fix IE6-8 createWithProxy $val: val引发的BUG)
+    if (!source || source.nodeType > 0 || (source.$id && source.$events)) {
+        return source
+    }
+    var $skipArray = Array.isArray(source.$skipArray) ? source.$skipArray : []
+    $skipArray.$special = $special || createMap() //强制要监听的属性
+    var $vmodel = {} //要返回的对象, 它在IE6-8下可能被偷龙转凤
+    $model = $model || {} //vmodels.$model属性
+    var $events = createMap() //vmodel.$events属性
+    var accessors = createMap() //监控属性
+    var initCallbacks = [] //初始化才执行的函数
+
+    $$skipArray.forEach(function (name) {
+        delete source[name]
+    })
+
+    for (var i in source) {
+        (function (name, val, accessor) {
+            $model[name] = val
+            if (!isObservable(name, val, $skipArray)) {
+                return //过滤所有非监控属性
+            }
+            //总共产生三种accessor
+            $events[name] = []
+            var valueType = avalon.type(val)
+            //总共产生三种accessor
+            if (valueType === "object" && isFunction(val.get) && Object.keys(val).length <= 2) {
+                accessor = makeComputedAccessor(name, val)
+                initCallbacks.push(accessor)
+            } else if (rcomplexType.test(valueType)) {
+                accessor = makeComplexAccessor(name, val, valueType)
+                initCallbacks.push(function () {
+                    var son = accessor._vmodel
+                    son.$events[subscribers] = this.$events[name]
+                })
+            } else {
+                accessor = makeSimpleAccessor(name, val)
+            }
+            accessors[name] = accessor
+        })(i, source[i])// jshint ignore:line
+    }
+
+    $vmodel = Object.defineProperties($vmodel, descriptorFactory(accessors)) //生成一个空的ViewModel
+    for (var name in source) {
+        if (!accessors[name]) {
+            $vmodel[name] = source[name]
+        }
+    }
+    //添加$id, $model, $events, $watch, $unwatch, $fire
+    $vmodel.$id = generateID()
+    $vmodel.$model = $model
+    $vmodel.$events = $events
+    for (i in EventBus) {
+        $vmodel[i] = EventBus[i]
+    }
+
+    Object.defineProperty($vmodel, "hasOwnProperty", {
+        value: function (name) {
+            return name in this.$model
+        },
+        writable: false,
+        enumerable: false,
+        configurable: true
+    })
+
+    initCallbacks.forEach(function (cb) { //收集依赖
+        cb.call($vmodel)
+    })
+    return $vmodel
+}
+
+//创建一个简单访问器
+function makeSimpleAccessor(name, value) {
+    function accessor(value) {
+        var oldValue = accessor._value
+        if (arguments.length > 0) {
+            if (stopRepeatAssign) {
+                return this
+            }
+            if (!isEqual(value, oldValue)) {
+                accessor.updateValue(this, value)
+                accessor.notify(this, value, oldValue)
+            }
+            return this
+        } else {
+            dependencyDetection.collectDependency(this, accessor)
+            return oldValue
+        }
+    }
+    accessorFactory(accessor, name)
+    accessor._value = value
+    return accessor;
+}
+
+///创建一个计算访问器
+function makeComputedAccessor(name, options) {
+    options.set = options.set || noop
+    function accessor(value) {//计算属性
+        var oldValue = accessor._value
+        var init = "_value" in accessor
+        if (arguments.length > 0) {
+            if (stopRepeatAssign) {
+                return this
+            }
+            accessor.set.call(this, value)
+            return this
+        } else {
+            //将依赖于自己的高层访问器或视图刷新函数（以绑定对象形式）放到自己的订阅数组中
+            dependencyDetection.collectDependency(this, accessor)
+            if (!accessor.digest) {
+                var vm = this
+                var id
+                accessor.digest = function () {
+                    accessor.updateValue = globalUpdateModelValue
+                    accessor.notify = noop
+                    accessor.call(vm)
+                    clearTimeout(id)//如果计算属性存在多个依赖项，那么等它们都更新了才更新视图
+                    id = setTimeout(function () {
+                        accessorFactory(accessor, accessor._name)
+                        accessor.call(vm)
+                    })
+                }
+            }
+            dependencyDetection.begin({
+                callback: function (vm, dependency) {//dependency为一个accessor
+                    var name = dependency._name
+                    if (dependency !== accessor) {
+                        var list = vm.$events[name]
+                        injectDependency(list, accessor.digest)
+                    }
+                }
+            })
+            try {
+                value = accessor.get.call(this)
+            } finally {
+                dependencyDetection.end()
+            }
+            if (oldValue !== value) {
+                accessor.updateValue(this, value)
+                init && accessor.notify(this, value, oldValue) //触发$watch回调
+            }
+            //将自己注入到低层访问器的订阅数组中
+            return value
+        }
+    }
+    accessor.set = options.set || noop
+    accessor.get = options.get
+    accessorFactory(accessor, name)
+    return accessor
+}
+
+//创建一个复杂访问器
+function makeComplexAccessor(name, initValue, valueType) {
+    function accessor(value) {
+        var oldValue = accessor._value
+        var son = accessor._vmodel
+        if (arguments.length > 0) {
+            if (stopRepeatAssign) {
+                return this
+            }
+            if (valueType === "array") {
+                var old = son._
+                son._ = []
+                son.clear()
+                son._ = old
+                son.pushArray(value)
+            } else if (valueType === "object") {
+                var $proxy = son.$proxy
+                var observes = this.$events[name] || []
+                son = accessor._vmodel = modelFactory(value)
+                son.$proxy = $proxy
+                if (observes.length) {
+                    observes.forEach(function (data) {
+                        if (data.rollback) {
+                            data.rollback() //还原 ms-with ms-on
+                            bindingHandlers[data.type](data, data.vmodels)
+                        }
+                    })
+                    son.$events[name] = observes
+                }
+            }
+            accessor.updateValue(this, son.$model)
+            accessor.notify(this, this._value, oldValue)
+            return this
+        } else {
+            dependencyDetection.collectDependency(this, accessor)
+            return son
+        }
+    }
+    accessorFactory(accessor, name)
+    accessor._vmodel = modelFactory(initValue)
+    return accessor
+}
+
+function globalUpdateValue(vmodel, value) {
+    vmodel.$model[this._name] = this._value = value
+}
+function globalUpdateModelValue(vmodel, value) {
+    vmodel.$model[this._name] = value
+}
+function globalNotify(vmodel, value, oldValue) {
+    var name = this._name
+    var array = vmodel.$events[name] //刷新值
+    if (array) {
+        fireDependencies(array) //同步视图
+        EventBus.$fire.call(vmodel, name, value, oldValue) //触发$watch回调
+    }
+}
+
+function accessorFactory(accessor, name) {
+    accessor._name = name
+    //同时更新_value与model
+    accessor.updateValue = globalUpdateValue
+    accessor.notify = globalNotify
+}
+//比较两个值是否相等
+var isEqual = Object.is || function (v1, v2) {
+    if (v1 === 0 && v2 === 0) {
+        return 1 / v1 === 1 / v2
+    } else if (v1 !== v1) {
+        return v2 !== v2
+    } else {
+        return v1 === v2
+    }
+}
+
+function isObservable(name, value, $skipArray) {
+    if (isFunction(value) || value && value.nodeType) {
+        return false
+    }
+    if ($skipArray.indexOf(name) !== -1) {
+        return false
+    }
+    var $special = $skipArray.$special
+    if (name && name.charAt(0) === "$" && !$special[name]) {
+        return false
+    }
+    return true
+}
+
+var descriptorFactory = function (obj) {
+    var descriptors = {}
+    for (var i in obj) {
+        descriptors[i] = {
+            get: obj[i],
+            set: obj[i],
+            enumerable: true,
+            configurable: true
+        }
+    }
+    return descriptors
+}
+
---
+++ src/14 virtualdom.js
@@ -0,0 +1,296 @@
+function VElement(element, parentNode) {
+    this.nodeType = 1
+    var vid = getUid(element)
+    this.vid = element.vid = String(vid)
+    this.nodeName = element.nodeName
+    this.className = element.className
+    this.attributes = []
+    this.childNodes = []
+    this.parentNode = parentNode
+    this.isVirtualdom = true
+    try {
+        if (parentNode) {
+            //    if(parentNode.childNodes.indexOf())
+            parentNode.appendChild(this)
+            //  console.log(parentNode)
+            //   avalon.Array.ensure(parentNode.childNodes, this)
+        }
+    } catch (e) {
+        console.log(e)
+    }
+    // this.style = {}
+}
+//属性,类名,样式,子节点
+function forEachElements(dom, callback) {
+    for (var i = 0, el; el = dom.childNodes[i++]; ) {
+        if (el.nodeType === 1) {
+            if (callback(el) === false) {
+                break
+            } else {
+                forEachElements(el, callback)
+            }
+        }
+    }
+}
+VElement.prototype = {
+    constructor: VElement,
+    queryVID: function (vid) {
+        var ret = null
+        forEachElements(this, function (el) {
+            if (el.vid === vid) {
+                ret = el
+                return false
+            }
+        })
+        return ret
+    },
+    queryID: function (id) {
+        var ret = null
+        forEachElements(this, function (el) {
+            if (el.id === id) {
+                ret = el
+                return false
+            }
+        })
+        return ret
+    },
+    queryTag: function (tagName) {
+        var ret = []
+        forEachElements(this, function (el) {
+            if (el.tagName === tagName) {
+                ret.push(el)
+            }
+        })
+        return ret
+    },
+    queryClass: function (className) {
+        var pattern = new RegExp("(^|\\s)" + className + "(\\s|$)")
+        var ret = []
+        forEachElements(this, function (el) {
+            if (pattern.test(el.className)) {
+                ret.push(el)
+            }
+        })
+        return ret
+    },
+    appendChild: function (node) {//node可以是元素节点,文档碎片或数组
+        var nodes = node.nodeType === 11 ? node.childNodes : Array.isArray(node) ? node : [node]
+        for (var i = 0, node; node = nodes[i++]; ) {
+            node.parentNode = this
+            this.childNodes.push(node)
+        }
+        return nodes
+    },
+    insertBefore: function (node, before) {//node可以是元素节点,文档碎片或数组
+        var nodes = node.nodeType === 11 ? node.childNodes : Array.isArray(node) ? node : [node]
+        var index = this.childNodes.indexOf(before)
+        if (index === -1) {
+            this.appendChild(nodes)
+        } else {
+            this.replaceChild(node, before, true)
+        }
+        return nodes
+    },
+    replaceChild: function (node, replaced, keepReplaced) {//node可以是元素节点,文档碎片或数组
+        var nodes = node.nodeType === 11 ? node.childNodes : Array.isArray(node) ? node : [node]
+        var children = this.childNodes
+        var index = children.indexOf(replaced)
+        if (index === -1)
+            return null
+        var args = keepReplaced ? [index, 0, replaced] : [index, 1]
+        for (var i = 0, el; el = nodes[i++]; ) {
+            el.parentNode = this
+            args.push(el)
+        }
+        Array.prototype.splice.apply(children, args)
+
+        return replaced
+    },
+    removeChild: function (elem) {
+        var children = this.childNodes
+        var index = children.indexOf(elem)
+        if (~index)
+            children.splice(index, 1)
+        return elem
+    },
+    getAttribute: function (name) {
+        var attrs = this.attributes
+        for (var i = 0, attr; attr = attrs[i++]; ) {
+            if (attr.name === name)
+                return attr.value
+        }
+    },
+    hasAttribute: function (name) {
+        var value = this.getAttribute(el, name)
+        return typeof value === "string"
+    },
+    getText: function () {
+        var ret = ""
+        var children = this.childNodes
+        for (var i = 0, el; el = children[i++]; ) {
+            ret += el.nodeValue
+        }
+        return ret
+    },
+    getValue: function () {
+        var blank = ""
+        var value = this.getAttribute("value")
+        switch (this.nodeName) {
+            case "TEXTAREA":
+                return this.getText()
+            case "OPTION":
+                return typeof value === "string" ? value : this.getText()
+            case "SELECT":
+            case "INPUT":
+            case "BUTTON":
+                return value || blank
+        }
+        return value
+    },
+    setStyle: function (name, value) {
+        var style = this.style || (this.style = {})
+        style[camelize(name)] = value
+    },
+    setAttribute: function (name, value) {
+        // zilong@2015-5-15: 在parse5序列化dom的attribute时，对于tabindex、colspan这类数字类型的属性，如果不转换为字符串，程序会崩溃
+        if (typeof value != 'string') {
+            value = String(1);
+        }
+        var attrs = this.attributes
+        for (var i = 0, attr; attr = attrs[i++]; ) {
+            if (attr.name === name) {
+                attr.value = value
+                return this
+            }
+        }
+        attrs.push({
+            name: name,
+            value: value
+        })
+        return this
+    },
+    removeAttribute: function (name) {
+        var attrs = this.attributes
+        for (var i = attrs.length, attr; attr = attrs[--i]; ) {
+            if (attr.name === name) {
+                attrs.splice(i, 1)
+                break
+            }
+        }
+        return this
+    },
+    setBoolAttribute: function (name, value) {
+        if (value) {
+            this.setAttribute(name, name)
+        } else {
+            this.removeAttribute(name)
+        }
+    }
+}
+
+function VComment(nodeValue) {
+    this.nodeType = 8
+    this.nodeName = "#comment"
+    this.nodeValue = nodeValue
+}
+
+function VText(nodeValue) {
+    this.nodeType = 3
+    this.nodeName = "#text"
+    this.nodeValue = nodeValue
+}
+
+function VDocumentFragment(nodeValue, parentNode) {
+    this.nodeType = 11
+    this.nodeName = "#document-fragment"
+    this.parentNode = parentNode
+}
+
+VDocumentFragment.prototype = {
+    constructor: VDocumentFragment
+}
+
+String("appendChild, removeChild,insertBefore,replaceChild").replace(/\w+/g, function (method) {
+    VDocumentFragment.prototype[method] = VElement.prototype[method]
+})
+//创建虚拟DOM的根节点
+var VTree = avalon.VTree = new VElement(root)
+//scanTag 遇到ms-controller会创建一个VElement添加到VTree
+
+var reID
+function globalRender() {
+    clearTimeout(reID)
+    reID = setTimeout(function () {
+        refreshTree()
+    }, 4)
+}
+function refreshTree() {
+    console.log("更新视图")
+    updateTree(VTree)
+}
+function querySelector(tag, vid, root) {
+    root = root || document
+//    if (root.querySelector) {
+//        console.log(tag + "[vid='" + vid + "']")
+//        root.querySelector(tag + "[vid=" + vid + "]")
+//    } else {
+    var nodes = root.getElementsByTagName(tag)
+    for (var i = 0, node; node = nodes[i++]; ) {
+        if (node.vid === vid)
+            return node
+    }
+    //   }
+}
+function updateTree(node) {
+    var diff = node.diffText || node.diffAttr || node.diffStyle
+    if (diff) {
+        var rnode = querySelector(node.nodeName, node.vid)
+    //    console.log("rnode", rnode, node.diffText)
+        if (!rnode)
+            return
+        if (node.diffText) {
+            //    console.log("更新{{}}")
+            var rnodes = rnode.childNodes
+            //    console.log(rnodes)
+            var vnodes = node.childNodes, vnode
+            for (var i = 0, el; el = rnodes[i]; i++) {
+                vnode = vnodes[i]
+                if (el.nodeType === 3 && vnode.nodeType === 3 && el.nodeValue !== vnode.nodeValue) {
+                    el.nodeValue = vnode.nodeValue
+                }
+            }
+        }
+    }
+    if (node.childNodes && node.childNodes.length) {
+        for (var i = 0, el; el = node.childNodes[i++]; ) {
+            if (el.nodeType === 1) {
+                updateTree(el)
+            }
+        }
+    }
+}
+function createVChild(realNode) {
+    var array = []
+    for (var i = 0, el; el = realNode.childNodes[i++]; ) {
+        switch (el.nodeType) {
+            case 1:
+                array.push(new VElement(el))
+                break
+            case 3:
+                array.push(new VText(el.nodeValue))
+                break
+            case 8:
+                array.push(new VComment(el.nodeValue))
+                break
+        }
+    }
+    return array
+}
+
+function getTextOrder(node, parent, el) {
+    for (var i = 0; el = parent.childNodes[i]; i++) {
+        if (node === el)
+            return i
+    }
+    return -1
+}
