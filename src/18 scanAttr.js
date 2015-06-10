025d498dba19027a10a99e7cca0a38a35936fe91
qincheng <qincheng@unitedstack.com>
2015-06-10 22:04:54 +0800

    update

--- src/18 scanAttr.js
+++ src/18 scanAttr.js
@@ -39,7 +39,8 @@ function scanAttr(elem, vmodels, match) {
                             priority: (priorityMap[type] || type.charCodeAt(0) * 10) + (Number(param.replace(/\D/g, "")) || 0)
                         }
                         if (type === "html" || type === "text") {
-                            var signature = generateID("v-" + type)
+                            var pid = buildTree(elem)
+                            var signature = "v-" + type + pid+".0"
                             var content = "<!--" + signature + ":" + value + "-->" +
                                     "<!--" + signature + ":" + value + ":end-->"
                             avalon(elem).innerHTML(content)
@@ -64,6 +65,7 @@ function scanAttr(elem, vmodels, match) {
                 elem.removeAttribute(arr[0])
                 elem.setAttribute(arr[1], arr[2])
             })
+            buildTree(elem)
             //http://bugs.jquery.com/ticket/7071
             //在IE下对VML读取type属性,会让此元素所有属性都变成<Failed>
             if (hasDuplex) {
--- src/18 scanNode.js
+++ src/18 scanNode.js
@@ -19,79 +19,100 @@ function scanNodeList(parent, vmodels) {
 //{{expr|html}} --> <!--v-thtml123213:expr--><!--v-text123213:expr:end-->
 function scanNodeArray(nodes, vmodels) {
     var bindings = []
-    var isVirtual = (nodes[0] || {}).isVirtual 
+    var firstChild = nodes[0] || {}
+    var isVirtual = firstChild.isVirtual == true
     var doc = isVirtual ? VDOC : DOC //使用何种文档对象来创建各种节点
+    var inDom = firstChild.nodeType === 1
     var nodeIndex = 0, parent
     for (var i = 0, node; node = nodes[i]; i++) {
-        if (node.nodeType === 1) {
-            nodeIndex++
-        } else if (node.nodeType === 3) {
-            if (rexpr.test(node.nodeValue)) {
-                var tokens = scanExpr(node.nodeValue)
-                var generateSignatures = false
-                outerLoop:
-                        for (var k = 0, token; token = tokens[k++]; ) {
-                    if (token.expr) {
-                        generateSignatures = true
-                        break outerLoop
-                    }
-                }
-                if (generateSignatures) {
-                    parent = parent || node.parentNode
-                    var pid = buildTree(parent)
-                    var fragment = doc.createDocumentFragment()
-                    for (k = 0; token = tokens[k++]; ) {
+        switch (node.nodeType) {
+            case 1:
+                nodeIndex++
+                break
+            case 3:
+                if (rexpr.test(node.nodeValue)) {
+                    var tokens = scanExpr(node.nodeValue)
+                    var generateSignatures = false
+                    outerLoop:
+                            for (var t = 0, token; token = tokens[t++]; ) {
                         if (token.expr) {
-                            nodeIndex++
-                            var signature = "v-" + token.type + pid + "." + nodeIndex
-                            token.signature = signature
-                            signature += ":" + token.value + (token.filters ? "|" + token.filters.join("|") : "")
-                            var start = doc.createComment(signature)
-                            var end = doc.createComment(signature + ":end")
-                            token.element = end
-                            bindings.push(token)
-                            fragment.appendChild(start)
-                            fragment.appendChild(end)
-                        } else {
-                            fragment.appendChild(doc.createTextNode(token.value))
+                            generateSignatures = true
+                            break outerLoop
                         }
                     }
-                    parent.replaceChild(fragment, node)
+                    if (generateSignatures) {//如果要生成占位用的注释节点
+                        parent = parent || node.parentNode
+                        var pid = buildTree(parent)
+                        var fragment = doc.createDocumentFragment()
+                        for (t = 0; token = tokens[t++]; ) {
+                            if (token.expr) {
+                                nodeIndex++
+                                var signature = "v-" + token.type + pid + "." + nodeIndex
+                                token.signature = signature
+                                signature += ":" + token.value + (token.filters ? "|" + token.filters.join("|") : "")
+                                var start = doc.createComment(signature)
+                                var end = doc.createComment(signature + ":end")
+                                token.element = end
+                                bindings.push(token)
+                                fragment.appendChild(start)
+                                fragment.appendChild(end)
+                            } else {
+                                fragment.appendChild(doc.createTextNode(token.value))
+                            }
+                        }
+                        parent.replaceChild(fragment, node)
+                    }
                 }
-            }
-        } else if (node.nodeType === 8) {
-            var nodeValue = node.nodeValue
-            if (nodeValue.slice(-4) !== ":end" && rvtext.test(nodeValue)) {
-                nodeIndex++
-                var content = nodeValue.replace(rvtext, function (a) {
-                    signature = a
-                    return ""
-                })
-                token = getToken(content)
-                token.element = node
-                token.signature = signature
-                token.type = nodeValue.indexOf("v-text") === 0 ? "text" : "html"
-                bindings.push(token)
-            }
+                break
+            case 8:
+                var nodeValue = node.nodeValue //如果后端渲染时已经生成好注释节点
+                if (nodeValue.slice(-4) !== ":end" && rvtext.test(nodeValue)) {
+                    nodeIndex++
+                    var content = nodeValue.replace(rvtext, function (a) {
+                        signature = a
+                        return ""
+                    })
+                    token = getToken(content)
+                    token.element = node
+                    token.signature = signature
+                    token.type = nodeValue.indexOf("v-text") === 0 ? "text" : "html"
+                    bindings.push(token)
+                }
+                break
         }
     }
 
     if (bindings.length) {
-        if(!isVirtual){
-          for( i = 0, node; node = parent.childNodes[i]; i++){
-          if (node.nodeType === 1) {
-             
-          } else if (node.nodeType === 3) {
-          } else if(node.nodeType === 8){
-              
-          }
+        if (!isVirtual && inDom) { //如果是扫描真实DOM树，那么我们需要在虚拟DOM树复制这一部分节点
+            vparent = VTree.queryVID(parent.getAttribute("data-vid"))
+            vparent.childNodes.length = 0
+            nodeIndex = 0
+            for (i = 0, node; node = parent.childNodes[i]; i++) {
+                switch (node.nodeType) {
+                    case 1:
+                        var vnode = VDOC.createElement(node.tagName, vparent)
+                        vnode.vid = pid + "." + nodeIndex++
+                        break
+                    case 3:
+                        var vnode = VDOC.createTextNode(node.valueNode)
+                        vparent.appendChild(vnode)
+                        break
+                    case 8:
+                        nodeValue = node.nodeValue
+                        if (nodeValue.slice(-4) !== ":end" && rvtext.test(nodeValue)) {
+                            nodeIndex++
+                        }
+                        var vnode = VDOC.createComment(nodeValue)
+                        vparent.appendChild(vnode)
+                        break
+
+                }
+            }
+            executeBindings(bindings, vmodels)
         }
-
-        executeBindings(bindings, vmodels)
-    }
-
-    for (var i = 0, node; node = nodes[i++]; ) {
-        scanElement(node, node.nodeType, vmodels)
+        for (i = 0; node = nodes[i++]; ) {
+            scanElement(node, node.nodeType, vmodels)
+        }
     }
 }
 function scanElement(node, nodeType, vmodels) {
