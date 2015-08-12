565ca09f8a706cdeebf14975feb18238751b2bbd
qincheng.zhong <qincheng.zhong@qunar.com>
2015-08-12 18:52:01 +0800

    update

--- src/19 directive/duplex.3.js
+++ src/19 directive/duplex.3.js
@@ -1,23 +1,23 @@
-duplexBinding.SELECT = function(element, evaluator, binding) {
+duplexBinding.SELECT = function (element, evaluator, binding) {
     var $elem = avalon(element)
 
-        function updateVModel() {
-            if ($elem.data("duplexObserve") !== false) {
-                var val = $elem.val() //字符串或字符串数组
-                if (Array.isArray(val)) {
-                    val = val.map(function(v) {
-                        return binding.pipe(v, binding, "get")
-                    })
-                } else {
-                    val = binding.pipe(val, binding, "get")
-                }
-                if (val + "" !== element.oldValue) {
-                    evaluator(val)
-                }
-                binding.changed.call(element, val, binding)
+    function updateVModel() {
+        if ($elem.data("duplexObserve") !== false) {
+            var val = $elem.val() //字符串或字符串数组
+            if (Array.isArray(val)) {
+                val = val.map(function (v) {
+                    return binding.pipe(v, binding, "get")
+                })
+            } else {
+                val = binding.pipe(val, binding, "get")
             }
+            if (val + "" !== element.oldValue) {
+                evaluator(val)
+            }
+            binding.changed.call(element, val, binding)
         }
-    binding.handler = function() {
+    }
+    binding.handler = function () {
         var val = evaluator()
         if (Array.isArray(val)) {
             if (!element.multiple) {
@@ -32,12 +32,14 @@ duplexBinding.SELECT = function(element, evaluator, binding) {
         val = Array.isArray(val) ? val.map(String) : val + ""
         if (val + "" !== element.oldValue) {
             $elem.val(val)
-            element.oldValue = val + "" 
+            element.oldValue = val + ""
         }
     }
+    var val = evaluator()
+    element.oldValue = Array.isArray(val) ? val.map(String) : val + ""
     binding.bound("change", updateVModel)
-    element.msCallback = function() {
+    renderedCallbacks.push(function () {
         binding.handler()
         binding.changed.call(element, evaluator(), binding)
-    }
+    })
 }
