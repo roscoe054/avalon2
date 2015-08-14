2d105ca6f6a076364cdf57d1a6ddd3dc654f4441
RubyLouvre <1669866773@qq.com>
2015-08-15 00:48:32 +0800

    fix animate

--- src/19 directive/effect.js
+++ src/19 directive/effect.js
@@ -150,6 +150,9 @@ function effectFactory(el, opts) {
     instance.el = el
     instance.driver = driver
     instance.useCss = driver !== "j"
+    if (instance.useCss) {
+        instance.cssEvent = driver === "t" ? transitionEndEvent : animationEndEvent
+    }
     instance.name = name
     instance.callbacks = avalon.effects[name] || {}
 
@@ -182,104 +185,88 @@ Effect.prototype = {
         if (document.hidden) {
             return
         }
+
         var me = this
         var el = me.el
+        callEffectHook(me, "abortLeave")
         callEffectHook(me, "beforeEnter")
         before(el) //  这里可能做插入DOM树的操作,因此必须在修改类名前执行
-
-        if (me.useCss) {
-            var curEnterClass = me.curEnterClass = me.enterClass()
-            me.eventName = me.driver === "t" ? transitionEndEvent : animationEndEvent
-            if (me.leaveCallback) {
-                el.removeEventListener(me.eventName, me.leaveCallback)
-                avalon(el).removeClass(me.curLeaveClass)
-                me.leaveCallback = null
+        var cssCallback = function (cancel) {
+            el.removeEventListener(me.cssEvent, me.cssCallback)
+            if (me.driver === "a") {
+                avalon(el).removeClass(me.cssClass)
             }
-
-            //注意,css动画的发生有几个必要条件
-            //1.定义了时长,2.有要改变的样式,3.必须插入DOM树 4.display不能等于none
-            //5.document.hide不能为true, 6transtion必须延迟一下才修改样式
-
-            me.enterCallback = function () {
-                el.removeEventListener(me.eventName, me.enterCallback)
-                if (me.driver === "a") {
-                    avalon(el).removeClass(curEnterClass)
-                }
+            if (cancel !== true) {
                 callEffectHook(me, "afterEnter")
                 after && after(el)
-                me.dispose()
+            }
+            me.dispose()
+        }
+        if (me.useCss) {
+            if (me.cssCallback) { //如果leave动画还没有完成,立即完成
+                me.cssCallback(true)
             }
 
+            me.cssClass = getEffectClass(me, "enter")
+            me.cssCallback = cssCallback
 
             me.update = function () {
-                el.addEventListener(me.eventName, me.enterCallback)
+                el.addEventListener(me.cssEvent, me.cssCallback)
                 if (me.driver === "t") {//transtion延迟触发
-                    avalon(el).removeClass(curEnterClass)
-                    console.log(new Date - 0)
+                    avalon(el).removeClass(me.cssClass)
                 }
-
             }
-            avalon(el).addClass(curEnterClass)//animation会立即触发
-            console.log(new Date - 0, el.className)
+            avalon(el).addClass(me.cssClass)//animation会立即触发
 
             effectBuffer.render(true)
             effectBuffer.queue.push(me)
 
         } else {
-            callEffectHook(this, "enter", function () {
-                callEffectHook(me, "afterEnter")
-                after && after(el)
-                me.dispose()
-            })
+            callEffectHook(me, "enter", cssCallback)
+
         }
     },
     leave: function (before, after) {
         if (document.hidden) {
             return
         }
-
         var me = this
         var el = me.el
+        callEffectHook(me, "abortEnter")
         callEffectHook(me, "beforeLeave")
-        if (me.useCss) {
-            var curLeaveClass = me.curLeaveClass = me.leaveClass()
-            me.eventName = me.driver === "t" ? transitionEndEvent : animationEndEvent
-
-            if (me.enterCallback) {
-                el.removeEventListener(me.eventName, me.enterCallback)
-                avalon(el).removeClass(me.curEnterClass)
-                me.enterCallback = null
-            }
-
-            me.leaveCallback = function () {
-                el.removeEventListener(me.eventName, me.leaveCallback)
-                before(el) //这里可能做移出DOM树操作,因此必须位于动画之后
-                avalon(el).removeClass(curLeaveClass)
+        var cssCallback = function (cancel) {
+            el.removeEventListener(me.cssEvent, me.cssCallback)
+            before(el) //这里可能做移出DOM树操作,因此必须位于动画之后
+            avalon(el).removeClass(me.cssClass)
+            if (cancel !== true) {
                 callEffectHook(me, "afterLeave")
                 after && after(el)
-                me.dispose()
             }
-            this.update = function () {
-                el.addEventListener(me.eventName, me.leaveCallback)
+            me.dispose()
+        }
+        if (me.useCss) {
+            if (me.cssCallback) { //如果leave动画还没有完成,立即完成
+                me.cssCallback(true)
             }
 
-            avalon(el).addClass(curLeaveClass)//animation立即触发
+            me.cssClass = getEffectClass(me, "leave")
+            me.cssCallback = cssCallback
+
+            me.update = function () {
+                el.addEventListener(me.cssEvent, me.cssCallback)
+            }
+
+            avalon(el).addClass(me.cssClass)//animation立即触发
             effectBuffer.render(true)
             effectBuffer.queue.push(me)
 
-
         } else {
-            callEffectHook(me, "leave", function () {
-                before(el)
-                callEffectHook(me, "afterLeave")
-                after && after(el)
-                me.dispose()
-            })
+            callEffectHook(me, "leave", cssCallback)
         }
 
     },
     dispose: function () {//销毁与回收到池子中
-        this.upate = this.el = this.driver = this.useCss = this.callbacks = null
+        this.update = this.cssCallback = null
         if (effectPool.unshift(this) > 100) {
             effectPool.pop()
         }
--- src/19 directive/visible.js
+++ src/19 directive/visible.js
@@ -24,7 +24,7 @@ avalon.directive("visible", {
             avalon.effect.apply(elem, 1, function () {
                 var data = elem.getAttribute("data-effect-driver") || "a"
                 if (/^[atn]/.test(data)) {
-                  
+                  console.log("update")
                  //   elem.style.display = ""//这里jQuery会自动处理
                     if (avalon(elem).css("display") === "none") {
                         elem.style.display = parseDisplay(elem.nodeName)
