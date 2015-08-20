6bf0a8d5a68a34ae7f53b811a3e34660d178b0b4
qincheng <qincheng@unitedstack.com>
2015-08-21 00:25:57 +0800

    update

---
+++ src/19 directive/duplex.js
@@ -0,0 +1,326 @@
+//双工绑定
+var rduplexType = /^(?:checkbox|radio)$/
+var rduplexParam = /^(?:radio|checked)$/
+var duplexBinding = avalon.directive("duplex", {
+    priority: 2000,
+    init: function (binding, hasCast) {
+        var elem = binding.element
+        var vmodels = binding.vmodels
+        binding.changed = getBindingCallback(elem, "data-duplex-changed", vmodels) || noop
+        var params = []
+        var casting = oneObject("string,number,boolean,checked")
+        if (elem.type === "radio" && binding.param === "") {
+            binding.param = "checked"
+        }
+        if (elem.msData) {
+            elem.msData["ms-duplex"] = binding.expr
+        }
+        binding.param.replace(rw20g, function (name) {
+            if (rduplexType.test(elem.type) && rduplexParam.test(name)) {
+                if (name === "radio")
+                    log("ms-duplex-radio已经更名为ms-duplex-checked")
+                name = "checked"
+                binding.isChecked = true
+                binding.xtype = "radio"
+            }
+            if (name === "bool") {
+                name = "boolean"
+                log("ms-duplex-bool已经更名为ms-duplex-boolean")
+            } else if (name === "text") {
+                name = "string"
+                log("ms-duplex-text已经更名为ms-duplex-string")
+            }
+            if (casting[name]) {
+                hasCast = true
+            }
+            avalon.Array.ensure(params, name)
+        })
+        if (!hasCast) {
+            params.push("string")
+        }
+        binding.param = params.join("-")
+        binding.changed = getBindingCallback(elem, "binding-duplex-changed", vmodels) || noop
+        if (!binding.xtype) {
+            binding.xtype = elem.tagName === "SELECT" ? "select" :
+                    elem.type === "checkbox" ? "checkbox" :
+                    /^change/.test(elem.getAttribute("data-duplex-event")) ? "change" :
+                    "input"
+        }
+        //===================绑定事件======================
+        binding.bound = function (type, callback) {
+            if (elem.addEventListener) {
+                elem.addEventListener(type, callback, false)
+            } else {
+                elem.attachEvent("on" + type, callback)
+            }
+            var old = binding.rollback
+            binding.rollback = function () {
+                elem.avalonSetter = null
+                avalon.unbind(elem, type, callback)
+                old && old()
+            }
+        }
+        var composing = false
+        function callback(value) {
+            binding.changed.call(this, value, binding)
+        }
+        function compositionStart() {
+            composing = true
+        }
+        function compositionEnd() {
+            composing = false
+        }
+        var updateVModel = function () {
+            if (composing) //处理中文输入法在minlengh下引发的BUG
+                return
+            var val = elem.oldValue = elem.value //防止递归调用形成死循环
+            var lastValue = binding.pipe(val, binding, "get")
+            if (avalon(elem).data("duplexObserve") !== false) {
+                binding.setter(lastValue)
+                callback.call(elem, lastValue)
+            }
+        }
+        switch (binding.xtype) {
+            case "radio":
+                binding.bound("click", function () {
+                    if (elem.data("duplexObserve") !== false) {
+                        var lastValue = binding.pipe(elem.value, binding, "get")
+                        binding.setter(lastValue)
+                        callback.call(elem, lastValue)
+                    }
+                })
+                break
+            case "checkbox":
+                binding.bound(W3C ? "change" : "click", function () {
+                    if (avalon(elem).data("duplexObserve") !== false) {
+                        var method = elem.checked ? "ensure" : "remove"
+                        var array = binding.evaluator()
+                        if (!Array.isArray(array)) {
+                            log("ms-duplex应用于checkbox上要对应一个数组")
+                            array = [array]
+                        }
+                        var val = binding.pipe(elem.value, binding, "get")
+                        avalon.Array[method](array, val)
+                        callback.call(elem, array)
+                    }
+                })
+                break
+            case "change":
+                binding.bound("change", updateVModel)
+                break
+            case "input":
+                if (!IEVersion) { // W3C
+                    binding.bound("input", updateVModel)
+                    //非IE浏览器才用这个
+                    binding.bound("compositionstart", compositionStart)
+                    binding.bound("compositionend", compositionEnd)
+                    binding.bound("DOMAutoComplete", updateVModel)
+                } else { //onpropertychange事件无法区分是程序触发还是用户触发
+                    // IE下通过selectionchange事件监听IE9+点击input右边的X的清空行为，及粘贴，剪切，删除行为
+                    if (IEVersion > 8) {
+                        binding.bound("input", updateVModel) //IE9使用propertychange无法监听中文输入改动
+                    } else {
+                        binding.bound("propertychange", function (e) { //IE6-8下第一次修改时不会触发,需要使用keydown或selectionchange修正
+                            if (e.propertyName === "value") {
+                                updateVModel()
+                            }
+                        })
+                    }
+                    binding.bound("dragend", function (e) {
+                        setTimeout(function () {
+                            updateVModel(e)
+                        })
+                    })
+                    //http://www.cnblogs.com/rubylouvre/archive/2013/02/17/2914604.html
+                    //http://www.matts411.com/post/internet-explorer-9-oninput/
+                }
+                break
+            case "select":
+                binding.bound("change", function () {
+                    if (avalon(elem).data("duplexObserve") !== false) {
+                        var val = avalon(elem).val() //字符串或字符串数组
+                        if (Array.isArray(val)) {
+                            val = val.map(function (v) {
+                                return binding.pipe(v, binding, "get")
+                            })
+                        } else {
+                            val = binding.pipe(val, binding, "get")
+                        }
+                        if (val + "" !== binding.oldValue) {
+                            avalon.setter(val)
+                        }
+                        callback.call(elem, val)
+                    }
+                })
+                break
+        }
+        if (binding.xtype === "input") {
+            watchValueInTimer(function () {
+                if (root.contains(elem)) {
+                    if (!elem.msFocus && binding.oldValue !== elem.value) {
+                        updateVModel()
+                    }
+                } else if (!elem.msRetain) {
+                    return false
+                }
+            })
+        }
+
+        elem.avalonSetter = updateVModel //#765
+        for (var i in avalon.vmodels) {
+            var v = avalon.vmodels[i]
+            v.$fire("avalon-ms-duplex-init", binding)
+        }
+        var cpipe = binding.pipe || (binding.pipe = pipe)
+        cpipe(null, binding, "init")
+    },
+    update: function (value) {
+        var elem = this.element, binding = this, curValue
+        switch (this.xtype) {
+            case "input":
+            case "change":
+                curValue = this.pipe(value, this, "set") + "" //fix #673
+                if (curValue !== this.oldValue) {
+                    elem.value = curValue
+                }
+                break
+            case "radio":
+                curValue = binding.isChecked ? !!value : value + "" === elem.value
+                if (IE6) {
+                    setTimeout(function () {
+                        //IE8 checkbox, radio是使用defaultChecked控制选中状态，
+                        //并且要先设置defaultChecked后设置checked
+                        //并且必须设置延迟
+                        elem.defaultChecked = curValue
+                        elem.checked = curValue
+                    }, 31)
+                } else {
+                    elem.checked = curValue
+                }
+                break
+            case "checkbox":
+                var array = [].concat(value) //强制转换为数组
+                curValue = this.pipe(elem.value, this, "get")
+                elem.checked = array.indexOf(curValue) > -1
+                break
+            case "select":
+                //必须变成字符串后才能比较
+                binding._value = value
+                renderedCallbacks.push(function () {
+                    var value = binding._value
+                    var curValue = Array.isArray(value) ? value.map(String) : value + ""
+                    avalon(elem).val(curValue)
+                    elem.oldValue = curValue + ""
+                    binding.changed.call(elem, curValue)
+                })
+                break
+        }
+        if (binding.xtype !== "select") {
+            binding.changed.call(elem, curValue)
+        }
+    }
+})
+
+
+function fixNull(val) {
+    return val == null ? "" : val
+}
+avalon.duplexHooks = {
+    checked: {
+        get: function (val, binding) {
+            return !binding.element.oldValue
+        }
+    },
+    string: {
+        get: function (val) { //同步到VM
+            return val
+        },
+        set: fixNull
+    },
+    "boolean": {
+        get: function (val) {
+            return val === "true"
+        },
+        set: fixNull
+    },
+    number: {
+        get: function (val, binding) {
+            var number = parseFloat(val)
+            if (-val === -number) {
+                return number
+            }
+            var arr = /strong|medium|weak/.exec(binding.element.getAttribute("data-duplex-number")) || ["medium"]
+            switch (arr[0]) {
+                case "strong":
+                    return 0
+                case "medium":
+                    return val === "" ? "" : 0
+                case "weak":
+                    return val
+            }
+        },
+        set: fixNull
+    }
+}
+
+function pipe(val, binding, action, e) {
+    binding.param.replace(rw20g, function (name) {
+        var hook = avalon.duplexHooks[name]
+        if (hook && typeof hook[action] === "function") {
+            val = hook[action](val, binding)
+        }
+    })
+    return val
+}
+
+var TimerID, ribbon = []
+
+avalon.tick = function (fn) {
+    if (ribbon.push(fn) === 1) {
+        TimerID = setInterval(ticker, 60)
+    }
+}
+
+function ticker() {
+    for (var n = ribbon.length - 1; n >= 0; n--) {
+        var el = ribbon[n]
+        if (el() === false) {
+            ribbon.splice(n, 1)
+        }
+    }
+    if (!ribbon.length) {
+        clearInterval(TimerID)
+    }
+}
+
+var watchValueInTimer = noop
+var rmsinput = /text|password|hidden/
+new function () { // jshint ignore:line
+    try { //#272 IE9-IE11, firefox
+        var setters = {}
+        var aproto = HTMLInputElement.prototype
+        var bproto = HTMLTextAreaElement.prototype
+        function newSetter(value) { // jshint ignore:line
+            setters[this.tagName].call(this, value)
+            if (rmsinput.test(this.type) && !this.msFocus && this.avalonSetter) {
+                this.avalonSetter()
+            }
+        }
+        var inputProto = HTMLInputElement.prototype
+        Object.getOwnPropertyNames(inputProto) //故意引发IE6-8等浏览器报错
+        setters["INPUT"] = Object.getOwnPropertyDescriptor(aproto, "value").set
+
+        Object.defineProperty(aproto, "value", {
+            set: newSetter
+        })
+        setters["TEXTAREA"] = Object.getOwnPropertyDescriptor(bproto, "value").set
+        Object.defineProperty(bproto, "value", {
+            set: newSetter
+        })
+    } catch (e) {
+        //在chrome 43中 ms-duplex终于不需要使用定时器实现双向绑定了
+        // http://updates.html5rocks.com/2015/04/DOM-attributes-now-on-the-prototype
+        // https://docs.google.com/document/d/1jwA8mtClwxI-QJuHT7872Z0pxpZz8PBkf2bGAbsUtqs/edit?pli=1
+        watchValueInTimer = avalon.tick
+    }
+} // jshint ignore:line
