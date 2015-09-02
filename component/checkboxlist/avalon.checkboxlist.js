2c34983a33e473de56931351dd529f44e381f1db
yuhao.ju <heiwu805@hotmail.com>
2015-09-02 19:09:21 +0800

    component checkboxlist：去掉模板html，使用户可分别配置两个部分的模板。去掉外部双绑，改为内部的双向绑定，只用onselect获取内部数据。

--- component/checkboxlist/avalon.checkboxlist.js
+++ component/checkboxlist/avalon.checkboxlist.js
@@ -1,4 +1,3 @@
-// avalon 1.3.6
 /**
  *
  * @cnName 复选框列表
@@ -6,49 +5,42 @@
  * @introduce
  *    <p>通过checkboxlist可以方便的实现选框的全选、全不选，并可通过简单配置进行选中操作的回调处理，也可以通过多种方式来提供渲染选项视图所需要的数据</p>
  */
+
 define(["avalon",
-    "text!./avalon.checkboxlist.html",
     "css!../style/oniui-common.css",
     "css!./avalon.checkboxlist.css"
-], function (avalon, template) {
-    var _interface = function () {
-    }
+], function (avalon) {
+
+    var _interface = function () {}
+
     avalon.component("oni:checkboxlist", {
-        data: [], //@config 所有选项值的集合，通过此数据来渲染初始视图。可以在组件初始化之前配置data，也可以在异步取得数据之后在配置。当同时配置了data、fetch且在绑定元素内部显示设置了要渲染的checkbox列表，则优先级顺序是：data>fetch>sub elements
+        // 内部方法
+        _clickOne: _interface,
+        _clickAll: _interface,
+
+        // 配置项
+        data: [], //@config 所有选项值的集合，通过此数据来渲染初始视图。
+        val: [], //@config 选中的checkbox value数组
         all: false, //@config 默认不选中所有选项
         alltext: "全部", //@config 显示"全部"按钮，方便进行全选或者全不选操作,不需要全选操作的话可以设置alltext为""
-        type: "", //@config 内置type为week时的data，用户只需配置type为week即可显示周一到周日的选项 
-        /**
-         * @config 通过配置fetch来获得要显示的数据，数据格式必须如下所示：
-         <pre class="brush:javascript;gutter:false;toolbar:false">
-         [
-         { text : '文字1' , value : 'w1' } ,
-         { text : '文字2' , value : 'w2' } ,
-         { text : '文字3' , value : 'w3' } ,
-         { text : '文字4' , value : 'w4' }
-         ]
-         </pre>
-         */
-        fetch: "",
-        $template: template,
-        /**
-         * @config {Function} 组件面板展开后的回调函数
-         * @param data {Array} checkboxlist的选项集合
-         * @param checkStatus {Boolean} 选中或者未选中的状态
-         * @param target {ElementObj} 触发事件的dom对象的引用
-         */
+        type: "", //@config 内置type为week时的data，用户只需配置type为week即可显示周一到周日的选项
+
+        // 回调方法
         onSelect: _interface,
-        vertical: true, //@config 如果希望选框水平排列则设置vertical为false，默认垂直排列
-        duplex: "",
+
+        // 模板
+        template_all: getTemplateString("checkAll"),
+        template_item: getTemplateString("checkItem"),
+        $template: "<ul class='oni-checkboxlist oni-checkboxlist-list oni-helper-clearfix'>"
+        + "{{template_all|html}}"
+        + "{{template_item|html}}"
+        + "</ul>",
+
         $construct: function (aaa, bbb, ccc) {
             var options = avalon.mix(aaa, bbb, ccc)
 
             if (!options.data.length) {
 
-                var fragment = document.createElement("div");
-                while (this.firstChild) {
-                    fragment.appendChild(this.firstChild);
-                }
                 switch (options.type) {
                     // 配置了type为week的话，使用组件默认的提供的data
                     case "week":
@@ -63,83 +55,77 @@ define(["avalon",
                         ];
                         break;
                     default:
-                        // 既未配置fetch自取data，也没配置type使用默认的data，就需要通过页面提供的html抽取出data
-                        var inputs = fragment.getElementsByTagName("input");
-                        var data = [];
-                        for (var i = 0; i < inputs.length; i++) {
-                            var input = inputs[i],
-                                li = input.parentNode,
-                                txt = "";
-                            // 获取离input最近的父级li元素
-                            while (li) {
-                                if (li.tagName === "LI") {
-                                    break;
-                                } else {
-                                    li = li.parentNode;
-                                }
-                            }
-                            txt = li.textContent || li.innerText;
-                            // trim掉li元素中文本两边的空格
-                            txt.replace(/^\s+/, "").replace(/\s+$/, "");
-                            // 将提取出来的数据保存在data中
-                            data.push({
-                                text: txt,
-                                value: input.value || txt
-                            });
-                        }
+                        break;
                 }
-
                 options.data = data
             }
 
-            options.$template = template.replace("MS_OPTIONS_DUPLEX", options.duplex);
-
             return options
         },
-        _clickOne: _interface,
-        _clickAll: _interface,
-        $init: function (vm, elem) {
 
-
+        $init: function (vm) {
 
         },
-        $ready: function (vm, elem) {
-            var ul = elem.getElementsByTagName("ul")[0],
-                lis = ul.children
 
-            ul.className += " oni-checkboxlist oni-checkboxlist-list oni-helper-clearfix";
+        $ready: function (vm) {
+            var listData = vm.$model.data
 
-            vm._clickAll = function(){
-                var someInputsChecked = false,
-                    allInputsChecked = true
+            vm._clickAll = function(e){
+                var selectedLen = vm.val.length
 
-                avalon.each(lis, function(i, li){
-                    if(i !== 0){
-                        var input = li.getElementsByTagName("input")[0]
-                        if(input.checked){
-                            someInputsChecked = true
-                        } else{
-                            allInputsChecked = false
-                        }
-                    }
-                })
-
-                if(!someInputsChecked || (someInputsChecked && !allInputsChecked)){
-                    avalon.each(lis, function(i, li){
-                        var input = li.getElementsByTagName("input")[0]
-                        input.checked = true
+                if(selectedLen < listData.length){
+                    avalon.each(listData, function(i, listItem){
+                        vm.val.push(listItem.value)
                     })
                 } else{
-                    avalon.each(lis, function(i, li){
-                        var input = li.getElementsByTagName("input")[0]
-                        input.checked = false
-                    })
+                    vm.val = []
                 }
+
+                setTimeout(function(){
+                    vm.onSelect(e, vm.val.$model)
+                }, 0)
+            }
+
+            vm._clickOne = function(e, index){
+                setTimeout(function(){
+                    vm.all = vm.val.length === listData.length
+                    vm.onSelect(e, vm.val.$model, index)
+                }, 0)
             }
         }
-
     })
 
+    function getTemplateString(key){
+
+        var tplStr_checkAll = '';
+        tplStr_checkAll += '<li class="oni-checkboxlist-item oni-checkboxlist-all" ms-if="!!alltext">';
+        tplStr_checkAll += '        <label ms-if="data.size()">';
+        tplStr_checkAll += '            <input type="checkbox"';
+        tplStr_checkAll += '                   ms-click="_clickAll($event)"';
+        tplStr_checkAll += '                   ms-duplex-checked="all"';
+        tplStr_checkAll += '                   \/> {{alltext}}';
+        tplStr_checkAll += '        <\/label>';
+        tplStr_checkAll += '    <\/li>';
+
+        var tplStr_checkItem = '';
+        tplStr_checkItem += '<li ms-repeat="data" class="oni-checkboxlist-item">';
+        tplStr_checkItem += '        <label>';
+        tplStr_checkItem += '            <input type="checkbox"';
+        tplStr_checkItem += '                   ms-click="_clickOne($event, $index)"';
+        tplStr_checkItem += '                   ms-duplex-string="val"';
+        tplStr_checkItem += '                   ms-attr-value="{{el.value||el.text}}"';
+        tplStr_checkItem += '                    \/>';
+        tplStr_checkItem += '            {{el.text|html}}';
+        tplStr_checkItem += '        <\/label>';
+        tplStr_checkItem += '    <\/li>';
+
+        var templateString = {
+            checkAll: tplStr_checkAll,
+            checkItem: tplStr_checkItem
+        }
+
+        return templateString[key]
+    }
 
     return avalon;
 });
