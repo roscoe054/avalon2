5449da8681f64c6ed911c1af16f9f1b86be6c0cf
qincheng.zhong <qincheng.zhong@qunar.com>
2015-10-08 20:08:05 +0800

    fix scanText BUG

--- src/18 scanText.js
+++ src/18 scanText.js
@@ -64,7 +64,7 @@ function scanText(textNode, vmodels, index) {
     var bindings = [],
     tokens = scanExpr(textNode.data)
     if (tokens.length) {
-        for (var i = 0; token = tokens[i++];) {
+        for (var i = 0, token; token = tokens[i++];) {
             var node = DOC.createTextNode(token.expr) //将文本转换为文本节点，并替换原来的文本节点
             if (token.type) {
                 token.expr = token.expr.replace(roneTime, function () {
